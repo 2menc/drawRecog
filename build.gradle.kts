c7d8d817df38db09871169ebd6a24dde1ee69b15
Domenico Valente <domenico.valente4@studio.unibo.it>
2026-07-29 17:03:23 +0200

    added automations

---
+++ build.gradle.kts
@@ -0,0 +1,95 @@
+plugins {
+    // Apply the java plugin to add support for Java
+    java
+
+    // Apply the application plugin to add support for building a CLI application
+    // You can run your app via task "run": ./gradlew run
+    application
+
+    /*
+     * Adds tasks to export a runnable jar.
+     * In order to create it, launch the "shadowJar" task.
+     * The runnable jar will be found in build/libs/projectname-all.jar
+     */
+    id("com.gradleup.shadow") version "9.3.1"
+}
+
+repositories { // Where to search for dependencies
+    mavenCentral()
+}
+
+dependencies {
+    // Suppressions for SpotBugs
+    compileOnly("com.github.spotbugs:spotbugs-annotations:4.9.8")
+
+    implementation("com.mysql:mysql-connector-j:9.3.0")
+
+    // Maven dependencies are composed by a group name, a name and a version, separated by colons
+    implementation("com.omertron:API-OMDB:1.5")
+    implementation("org.jooq:jool:0.9.15")
+    implementation("com.formdev:flatlaf:3.7.1")
+    implementation("org.yaml:snakeyaml:2.2")
+
+    /*
+     * Simple Logging Facade for Java (SLF4J)
+     * See: http://www.slf4j.org/
+     */
+    val slf4jVersion = "2.0.17"
+    implementation("org.slf4j:slf4j-api:$slf4jVersion")
+    // Logback backend for SLF4J
+    runtimeOnly("ch.qos.logback:logback-classic:1.5.24")
+
+    // JUnit API and testing engine
+    testImplementation(platform("org.junit:junit-bom:6.0.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation("org.assertj:assertj-core:3.27.3")
+
+}
+
+application {
+    // Define the main class for the application.
+    mainClass.set("client.Launcher")
+}
+
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform() // Enables the engine of JUnit 5/6
+    testLogging { // Additional Options
+        // Display all events (test started, succeeded, failed...)
+        events(*org.gradle.api.tasks.testing.logging.TestLogEvent.entries.toTypedArray())
+        showStandardStreams = true // Show the standard output
+    }
+}
+
+tasks.withType<Javadoc>().configureEach {
+    options {
+        if (this is StandardJavadocDocletOptions) {
+            addStringOption("Xdoclint:all,-missing", "-quiet")
+        }
+    }
+}
+
+
+// ==========================================
+// CUSTOM SERVER AND CLIENT AUTOMATIONS
+// ==========================================
+
+tasks.register<Exec>("server") {
+    group = "application"
+    description = "Avvia il server Python per il Machine Learning"
+
+    val venvPython = file("v_env/Scripts/python.exe")
+    val pythonExec = if (venvPython.exists()) venvPython.absolutePath else "py"
+
+    commandLine(pythonExec, "-u", "src/main/python/server/listener.py")
+}
+
+tasks.register<JavaExec>("client") {
+    group = "application"
+    description = "run java client and gui"
+    
+    classpath = sourceSets["main"].runtimeClasspath
+    
+    mainClass.set("client.Launcher") 
+}
